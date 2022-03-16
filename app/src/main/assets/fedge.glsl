9c45b4e60e372e30aeffab08b940d878fc4465c1
netman <netman>
2022-03-16 04:13:58 +0100

    rework the edge detect too while i'm at it

--- app/src/main/assets/fedge.glsl
+++ app/src/main/assets/fedge.glsl
@@ -7,21 +7,32 @@ uniform vec2 texSize;
 
 void main(void) {
 	vec2 ts = 1.0 / texSize;
-
-	vec4 n[9];
-	n[0] = texture2D(sTexture, texCoord + vec2(-ts.x, -ts.y));
-	n[1] = texture2D(sTexture, texCoord + vec2(0.0, -ts.y));
-	n[2] = texture2D(sTexture, texCoord + vec2(ts.x, -ts.y));
-	n[3] = texture2D(sTexture, texCoord + vec2(-ts.x, 0.0));
-	n[4] = texture2D(sTexture, texCoord);
-	n[5] = texture2D(sTexture, texCoord + vec2(ts.x, 0.0));
-	n[6] = texture2D(sTexture, texCoord + vec2(-ts.x, ts.y));
-	n[7] = texture2D(sTexture, texCoord + vec2(0.0, ts.y));
-	n[8] = texture2D(sTexture, texCoord + vec2(ts.x, ts.y));
-
-	vec4 edgex = n[2] + 2.0 * n[5] + n[8] - (n[0] + 2.0 * n[3] + n[6]);
-	vec4 edgey = n[0] + 2.0 * n[1] + n[2] - (n[6] + 2.0 * n[7] + n[8]);
+	vec4 a = texture2D(sTexture, texCoord + vec2(-ts.x, -ts.y));
+	vec4 b = texture2D(sTexture, texCoord + vec2(0.0, -ts.y)) * 2.0;
+	vec4 c = texture2D(sTexture, texCoord + vec2(ts.x, -ts.y));
+	vec4 d = texture2D(sTexture, texCoord + vec2(-ts.x, 0.0)) * 2.0;
+	vec4 e = texture2D(sTexture, texCoord + vec2(ts.x, 0.0)) * 2.0;
+	vec4 f = texture2D(sTexture, texCoord + vec2(-ts.x, ts.y));
+	vec4 g = texture2D(sTexture, texCoord + vec2(0.0, ts.y)) * 2.0;
+	vec4 h = texture2D(sTexture, texCoord + vec2(ts.x, ts.y));
+	vec4 edgex = -a + -d + -f + c + e + h;
+	vec4 edgey = a + b + c + -f + -g + -h;
 	vec4 sobel = sqrt((edgex * edgex) + (edgey * edgey));
-
 	gl_FragColor = vec4(1.0, 1.0, 1.0, dot(sobel.rgb, vec3(0.299, 0.587, 0.114)));
 }
+
+/* Another option:
+void main(void) {
+	vec2 ts = 1.0 / texSize;
+	vec4 px = texture2D(sTexture, texCoord) * 8.0;
+	px -= texture2D(sTexture, texCoord + vec2(-ts.x, -ts.y));
+	px -= texture2D(sTexture, texCoord + vec2(0.0, -ts.y));
+	px -= texture2D(sTexture, texCoord + vec2(ts.x, -ts.y));
+	px -= texture2D(sTexture, texCoord + vec2(-ts.x, 0.0));
+	px -= texture2D(sTexture, texCoord + vec2(ts.x, 0.0));
+	px -= texture2D(sTexture, texCoord + vec2(-ts.x, ts.y));
+	px -= texture2D(sTexture, texCoord + vec2(0.0, ts.y));
+	px -= texture2D(sTexture, texCoord + vec2(ts.x, ts.y));
+	gl_FragColor = vec4(1.0, 1.0, 1.0, dot(px.rgb, vec3(0.299, 0.587, 0.114)));
+}
+*/
--- app/src/main/assets/fshader.glsl
+++ app/src/main/assets/fshader.glsl
@@ -8,12 +8,13 @@ uniform float sharpening;
 
 void main(void) {
 	vec4 px = texture2D(sTexture, texCoord);
-	if (sharpening > 0.0) {
+	if (sharpening > 0.0) { /* Awesome page: https://setosa.io/ev/image-kernels/ */
+		vec2 ts = 1.0 / texSize;
 		vec4 spx = px * 5.0;
-		spx -= texture2D(sTexture, texCoord + vec2(0.0, -1.0) / texSize);
-		spx -= texture2D(sTexture, texCoord + vec2(0.0, 1.0) / texSize);
-		spx -= texture2D(sTexture, texCoord + vec2(-1.0, 0) / texSize);
-		spx -= texture2D(sTexture, texCoord + vec2(1.0, 0) / texSize);
+		spx -= texture2D(sTexture, texCoord + vec2(0.0, -ts.y));
+		spx -= texture2D(sTexture, texCoord + vec2(0.0, ts.y));
+		spx -= texture2D(sTexture, texCoord + vec2(-ts.x, 0));
+		spx -= texture2D(sTexture, texCoord + vec2(ts.x, 0));
 		px = mix(px, spx, sharpening);
 	}
 	gl_FragColor = px;
