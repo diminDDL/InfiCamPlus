75e8cd8a8d7dc06a3319ec1593dc0b6493b0d90b
netman <netman>
2022-03-25 07:10:38 +0100

    simplify the battery indicator

--- app/src/main/java/be/ntmn/inficam/BatteryLevel.java
+++ app/src/main/java/be/ntmn/inficam/BatteryLevel.java
@@ -2,76 +2,62 @@ package be.ntmn.inficam;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.ColorFilter;
-import android.graphics.PixelFormat;
-import android.graphics.drawable.Drawable;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
 
-public class BatteryLevel extends androidx.appcompat.widget.AppCompatTextView {
-	private class BatteryDrawable extends Drawable {
-		Drawable battery;
-		int scale = 1, level = 0;
+public class BatteryLevel extends View {
+	private final static int batColor = Color.WHITE;
+	private final static int textColor = Color.BLACK;
+	private final static int emptyColor = Color.rgb(150, 150, 150);
+	private final static int fullColor = Color.rgb(20, 200, 20);
 
-		BatteryDrawable(Drawable battery) {
-			this.battery = battery;
-		}
+	private final Paint paint = new Paint();
+	private int scale = 1, level = 0;
 
-		@Override
-		public void draw(@NonNull Canvas canvas) {
-			battery.setBounds(0, 0, BatteryLevel.this.getWidth(), BatteryLevel.this.getHeight());
-			//battery.setLevel(50 * 10000 / 100);
-			battery.draw(canvas);
-		}
-
-		@Override
-		public void setAlpha(int i) { /* Ignored. */ }
-
-		@Override
-		public void setColorFilter(@Nullable ColorFilter colorFilter) { /* Ignored. */ }
-
-		@Override
-		public int getOpacity() {
-			return PixelFormat.OPAQUE;
-		}
-
-		public void setLevel(int scale, int level) {
-			this.scale = scale;
-			this.level = level;
-			invalidateSelf();
-		}
-	}
-
-	private Context ctx;
-	private BatteryDrawable drawable;
-
-	public BatteryLevel(Context context) {
-		super(context);
-		init(context);
-	}
-
-	public BatteryLevel(Context context, @Nullable AttributeSet attrs) {
-		super(context, attrs);
-		init(context);
-	}
-
+	public BatteryLevel(Context context) { super(context); }
+	public BatteryLevel(Context context, @Nullable AttributeSet attrs) { super(context, attrs); }
 	public BatteryLevel(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
-		init(context);
-	}
-
-	private void init(Context ctx) {
-		this.ctx = ctx;
-		Drawable battery = ContextCompat.getDrawable(ctx, R.drawable.ic_baseline_battery);
-		drawable = new BatteryDrawable(battery);
-		setBackgroundDrawable(drawable);
 	}
 
 	public void setLevel(int scale, int level) {
-		drawable.setLevel(scale, level);
-		setText(ctx.getString(R.string.batlevel, level * 100 / scale));
+		this.scale = scale;
+		this.level = level;
+		invalidate();
+	}
+
+	/* I tried hard to do this the "proper" way with XML drawables. I've concluded they are
+	 *   complete and utter typhus infected garbage and we're far better off doing this.
+	 */
+	@Override
+	public void draw(@NonNull Canvas canvas) {
+		super.draw(canvas);
+		int w = getWidth();
+		int h = getHeight();
+		paint.setAntiAlias(true);
+		canvas.save();
+		canvas.translate(0, h / 2.0f);
+		canvas.scale(w / 24.0f, w / 24.0f);
+		paint.setColor(batColor);
+		canvas.drawRoundRect(2, -5, 20, 5, 1, 1, paint);
+		canvas.drawRect(19, -2, 22, 2, paint);
+		paint.setColor(emptyColor);
+		canvas.drawRect(4, -3, 18, 3, paint);
+		paint.setColor(fullColor);
+		float lvl = (float) level / scale;
+		canvas.drawRect(4, -3, 4 + 14 * lvl, 3, paint);
+		paint.setColor(textColor);
+		paint.setTextSize(5.5f);
+		paint.setFakeBoldText(true);
+		paint.setTypeface(Typeface.DEFAULT_BOLD);
+		paint.setTextAlign(Paint.Align.CENTER);
+		canvas.drawText((level * 100 / scale) + "%", 4 + 7, 2, paint);
+		canvas.restore();
 	}
 }
