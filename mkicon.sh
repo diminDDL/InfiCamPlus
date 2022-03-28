8ee68e9b0d266cc06306029450abeb94056d2e7f
netman <netman>
2022-03-28 20:38:00 +0200

    iconnnzz

--- mkicon.sh
+++ mkicon.sh
@@ -10,3 +10,15 @@ convert -background none -resize 72x72 logo_round.svg app/src/main/res/mipmap-hd
 convert -background none -resize 96x96 logo_round.svg app/src/main/res/mipmap-xhdpi/ic_launcher_round.png
 convert -background none -resize 144x144 logo_round.svg app/src/main/res/mipmap-xxhdpi/ic_launcher_round.png
 convert -background none -resize 192x192 logo_round.svg app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png
+
+convert -background none -resize 108x108 logo_fg.svg app/src/main/res/mipmap-mdpi/ic_launcher_fg.png
+convert -background none -resize 162x162 logo_fg.svg app/src/main/res/mipmap-hdpi/ic_launcher_fg.png
+convert -background none -resize 216x216 logo_fg.svg app/src/main/res/mipmap-xhdpi/ic_launcher_fg.png
+convert -background none -resize 324x324 logo_fg.svg app/src/main/res/mipmap-xxhdpi/ic_launcher_fg.png
+convert -background none -resize 432x432 logo_fg.svg app/src/main/res/mipmap-xxxhdpi/ic_launcher_fg.png
+
+convert -background none -resize 108x108 logo_bg.svg app/src/main/res/mipmap-mdpi/ic_launcher_bg.png
+convert -background none -resize 162x162 logo_bg.svg app/src/main/res/mipmap-hdpi/ic_launcher_bg.png
+convert -background none -resize 216x216 logo_bg.svg app/src/main/res/mipmap-xhdpi/ic_launcher_bg.png
+convert -background none -resize 324x324 logo_bg.svg app/src/main/res/mipmap-xxhdpi/ic_launcher_bg.png
+convert -background none -resize 432x432 logo_bg.svg app/src/main/res/mipmap-xxxhdpi/ic_launcher_bg.png
