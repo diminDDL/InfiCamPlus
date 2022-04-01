7edda1657a7d86ca1188b5ba17a63863b241f481
netman <netman>
2022-04-01 06:41:14 +0200

    simplify fxaa shader a bit

--- app/src/main/assets/ffxaa.glsl
+++ app/src/main/assets/ffxaa.glsl
@@ -5,18 +5,20 @@ uniform samplerExternalOES sTexture;
 varying vec2 texCoord;
 uniform vec2 texSize;
 
-#define FXAA_SUBPIX_SHIFT (0.0) // (1.0 / 4.0) originally
+#define FXAA_SUBPIX_DISTANCE (0.5)
 #define FXAA_SPAN_MAX (1.0) // (8.0) originally
 #define FXAA_REDUCE_MIN (0.0) // (1.0 / 128.0) originally
 #define FXAA_REDUCE_MUL (0.0) // (1.0 / 8.0) originally
 
 /* Adapted from: https://www.geeks3d.com/20110405/ */
-vec3 FxaaPixelShader(vec4 posPos, vec2 rcpFrame) {
-	vec3 rgbNW = texture2D(sTexture, posPos.zw).xyz;
-	vec3 rgbNE = texture2D(sTexture, posPos.zw + vec2(1.0, 0.0) * rcpFrame).xyz;
-	vec3 rgbSW = texture2D(sTexture, posPos.zw + vec2(0.0, 1.0) * rcpFrame).xyz;
-	vec3 rgbSE = texture2D(sTexture, posPos.zw + vec2(1.0, 1.0) * rcpFrame).xyz;
-	vec3 rgbM  = texture2D(sTexture, posPos.xy).xyz;
+void main(void) {
+	vec2 rcpFrame = 1.0 / texSize;
+	vec2 pOffset = rcpFrame * FXAA_SUBPIX_DISTANCE;
+	vec3 rgbNW = texture2D(sTexture, texCoord + vec2(-pOffset.x, -pOffset.y)).xyz;
+	vec3 rgbNE = texture2D(sTexture, texCoord + vec2(pOffset.x, -pOffset.y)).xyz;
+	vec3 rgbSW = texture2D(sTexture, texCoord + vec2(-pOffset.x, pOffset.y)).xyz;
+	vec3 rgbSE = texture2D(sTexture, texCoord + vec2(pOffset.x, pOffset.y)).xyz;
+	vec3 rgbM  = texture2D(sTexture, texCoord).xyz;
 	/*---------------------------------------------------------*/
 	vec3 luma = vec3(0.299, 0.587, 0.114);
 	float lumaNW = dot(rgbNW, luma);
@@ -35,25 +37,16 @@ vec3 FxaaPixelShader(vec4 posPos, vec2 rcpFrame) {
 	float dirReduce = max((lumaNW + lumaNE + lumaSW + lumaSE) * (0.25 * FXAA_REDUCE_MUL),
 			FXAA_REDUCE_MIN);
 	float rcpDirMin = 1.0 / (min(abs(dir.x), abs(dir.y)) + dirReduce);
-	dir = min(vec2(FXAA_SPAN_MAX, FXAA_SPAN_MAX),
-			max(vec2(-FXAA_SPAN_MAX, -FXAA_SPAN_MAX), dir * rcpDirMin)) * rcpFrame.xy;
+	dir = clamp(dir * rcpDirMin, -FXAA_SPAN_MAX, FXAA_SPAN_MAX) * rcpFrame;
 	/*--------------------------------------------------------*/
 	vec3 rgbA = (1.0 / 2.0) * (
-			texture2D(sTexture, posPos.xy + dir * (1.0 / 3.0 - 0.5)).xyz +
-			texture2D(sTexture, posPos.xy + dir * (2.0 / 3.0 - 0.5)).xyz);
+			texture2D(sTexture, texCoord + dir * (1.0 / 3.0 - 0.5)).xyz +
+			texture2D(sTexture, texCoord + dir * (2.0 / 3.0 - 0.5)).xyz);
 	vec3 rgbB = rgbA * (1.0 / 2.0) + (1.0 / 4.0) * (
-			texture2D(sTexture, posPos.xy + dir * (0.0 / 3.0 - 0.5)).xyz +
-			texture2D(sTexture, posPos.xy + dir * (3.0 / 3.0 - 0.5)).xyz);
+			texture2D(sTexture, texCoord + dir * (0.0 / 3.0 - 0.5)).xyz +
+			texture2D(sTexture, texCoord + dir * (3.0 / 3.0 - 0.5)).xyz);
 	float lumaB = dot(rgbB, luma);
 	if ((lumaB < lumaMin) || (lumaB > lumaMax))
-		return rgbA;
-	return rgbB;
-}
-
-void main(void) {
-	vec2 rcpFrame = 1.0 / texSize;
-	vec4 posPos;
-	posPos.xy = texCoord;
-	posPos.zw = texCoord - rcpFrame * (0.5 + FXAA_SUBPIX_SHIFT);
-	gl_FragColor = vec4(FxaaPixelShader(posPos, rcpFrame), 1.0);
+		gl_FragColor = vec4(rgbA, 1.0);
+	else gl_FragColor = vec4(rgbB, 1.0);
 }
