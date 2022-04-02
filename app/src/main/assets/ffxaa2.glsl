0dbd5f85b85cb2c492258efe494cb7197bea74f3
netman <netman>
2022-04-02 22:20:19 +0200

    experiments

--- app/src/main/assets/ffxaa2.glsl
+++ app/src/main/assets/ffxaa2.glsl
@@ -19,8 +19,9 @@ vec2 rotate(vec2 v, float a) {
 }
 
 void main(void) {
-	vec2 span = 1.0 / texSize * 0.5;
+	vec2 span = 1.0 / texSize/* * 0.5*/;
 	vec3 cOrg = texture2D(sTexture, texCoord).rgb;
+	//vec3 cNear = texture2D(sTexture, (floor(texCoord * texSize) + 0.5) / texSize).rgb;
 	vec3 cW = texture2D(sTexture, texCoord + vec2(-1.0, 0.0) * span).rgb;
 	vec3 cE = texture2D(sTexture, texCoord + vec2(1.0, 0.0) * span).rgb;
 	vec3 cN = texture2D(sTexture, texCoord + vec2(0.0, -1.0) * span).rgb;
@@ -34,13 +35,15 @@ void main(void) {
 	vec3 edgex = cNW + cN * 2.0 + cNE + -cSW + -cS * 2.0 + -cSE;
 	vec3 edgey = -cNW + -cW * 2.0 + -cSW + cNE + cE * 2.0 + cSE;
 	vec3 sobel = sqrt((edgex * edgex) + (edgey * edgey));
-	vec3 e = (cNW + cSE) - (cNE + cSW);
+	vec3 e = abs((cNW + cSE) - (cNE + cSW));
 
 	vec2 iuv = floor(texCoord * texSize) + 0.5;
 	vec2 fuv = fract(texCoord * texSize) - 0.5;
 	//fuv = rotate(fuv, M_PI * 0.25);
-	vec3 col = (cOrg * 4.0 + /*(cW + cE + cN + cS) * 2.0 + */(cNW + cNE + cSW + cSE)) / 8.0;
-	col = mix(cOrg, col, clamp(abs(luma(sobel)), 0.0, 1.0));
+	vec3 col = (cOrg * 4.0 + (cW + cE + cN + cS) * 2.0 + (cNW + cNE + cSW + cSE)) / 16.0;
+	//vec3 col = (cOrg + (cW + cE + cN + cS) + (cNW + cNE + cSW + cSE)) / 9.0;
+	//col = mix(cOrg, col, clamp(luma(e), 0.0, 1.0));
 	//gl_FragColor = vec4(vec3(abs(luma(sobel))), 1.0);
+	//gl_FragColor = vec4(cOrg, 1.0);
 	gl_FragColor = vec4(col, 1.0);
 }
