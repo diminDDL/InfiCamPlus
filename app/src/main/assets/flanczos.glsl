9ad1ebffb870a87f26ebe6563beb21b0a6049134
netman <netman>
2022-04-03 18:33:34 +0200

    writing down some thoughts that may or may not make sense

--- app/src/main/assets/flanczos.glsl
+++ app/src/main/assets/flanczos.glsl
@@ -20,10 +20,12 @@ float lanc2(float x) {
 float lanc2(vec2 x) { return lanc2(x.x) * lanc2(x.y); }
 
 // Does the aliasing happen because the circular kernel doesn't combine to 1 diagonally?
-// Can we fix this with a squareish kernel?
+// Can we fix this with a squareish kernel? i think not
 // Or do we just need to bandlimit or upscale in multiple steps?
 //   Scaling in multiple steps in gimp seems to avoid aliasing, but also adds some blur.
 //   Median filter really helps with that blur. Is there an optimum step size with least blur?
+// Are the higher frequency components introduced only diagonal? should we blur only diagonally?
+// Is 3x the right amount to scale in steps? 1.5 seems to give best results in gimp
 
 void main(void) {
 	vec2 ts = 1.0 / texSize;
@@ -51,29 +53,27 @@ void main(void) {
 	vec3 cEEN = texture2D(sTexture, (iuv + vec2(2.0, -1.0)) * ts).rgb;
 	vec3 cEES = texture2D(sTexture, (iuv + vec2(2.0, 1.0)) * ts).rgb;
 
-	fuv = -fuv;
 	float w0 = lanc2(fuv);
-	float wN = lanc2(fuv + vec2(0.0, -1.0));
-	float wS = lanc2(fuv + vec2(0.0, 1.0));
-	float wW = lanc2(fuv + vec2(-1.0, 0.0));
-	float wE = lanc2(fuv + vec2(1.0, 0.0));
-	float wNW = lanc2(fuv + vec2(-1.0, -1.0));
-	float wNE = lanc2(fuv + vec2(1.0, -1.0));
-	float wSW = lanc2(fuv + vec2(-1.0, 1.0));
-	float wSE = lanc2(fuv + vec2(1.0, 1.0));
-
-	float wNN = lanc2(fuv + vec2(0.0, -2.0));
-	float wSS = lanc2(fuv + vec2(0.0, 2.0));
-	float wWW = lanc2(fuv + vec2(-2.0, 0.0));
-	float wEE = lanc2(fuv + vec2(2.0, 0.0));
-	float wNNW = lanc2(fuv + vec2(-1.0, -2.0));
-	float wNNE = lanc2(fuv + vec2(1.0, -2.0));
-	float wSSW = lanc2(fuv + vec2(-1.0, 2.0));
-	float wSSE = lanc2(fuv + vec2(1.0, 2.0));
-	float wWWN = lanc2(fuv + vec2(-2.0, -1.0));
-	float wWWS = lanc2(fuv + vec2(-2.0, 1.0));
-	float wEEN = lanc2(fuv + vec2(2.0, -1.0));
-	float wEES = lanc2(fuv + vec2(2.0, 1.0));
+	float wN = lanc2(fuv - vec2(0.0, -1.0));
+	float wS = lanc2(fuv - vec2(0.0, 1.0));
+	float wW = lanc2(fuv - vec2(-1.0, 0.0));
+	float wE = lanc2(fuv - vec2(1.0, 0.0));
+	float wNW = lanc2(fuv - vec2(-1.0, -1.0));
+	float wNE = lanc2(fuv - vec2(1.0, -1.0));
+	float wSW = lanc2(fuv - vec2(-1.0, 1.0));
+	float wSE = lanc2(fuv - vec2(1.0, 1.0));
+	float wNN = lanc2(fuv - vec2(0.0, -2.0));
+	float wSS = lanc2(fuv - vec2(0.0, 2.0));
+	float wWW = lanc2(fuv - vec2(-2.0, 0.0));
+	float wEE = lanc2(fuv - vec2(2.0, 0.0));
+	float wNNW = lanc2(fuv - vec2(-1.0, -2.0));
+	float wNNE = lanc2(fuv - vec2(1.0, -2.0));
+	float wSSW = lanc2(fuv - vec2(-1.0, 2.0));
+	float wSSE = lanc2(fuv - vec2(1.0, 2.0));
+	float wWWN = lanc2(fuv - vec2(-2.0, -1.0));
+	float wWWS = lanc2(fuv - vec2(-2.0, 1.0));
+	float wEEN = lanc2(fuv - vec2(2.0, -1.0));
+	float wEES = lanc2(fuv - vec2(2.0, 1.0));
 
 	vec3 col = c0 * w0 + cN * wN + cS * wS + cW * wW + cE * wE;
 	col += cNW * wNW + cNE * wNE + cSW * wSW + cSE * wSE;
