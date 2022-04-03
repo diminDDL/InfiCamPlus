f316840a216764c22eeaefe19844483b689f3f1d
netman <netman>
2022-04-03 04:25:26 +0200

    t works but not entirely right

--- app/src/main/assets/flanczos.glsl
+++ app/src/main/assets/flanczos.glsl
@@ -1,22 +1,25 @@
 #extension GL_OES_EGL_image_external : require
 
-precision highp float;
+precision mediump float;
 uniform samplerExternalOES sTexture;
 varying vec2 texCoord;
 uniform vec2 texSize;
 
-#define M_PI (3.1415926535897932384626433832795)
+#define M_PI (radians(180.0))
 
 float lanc2(float x) {
+	float a = 2.0;
 	if (x == 0.0)
 		return 1.0;
-	return (2.0 * sin(M_PI * x) * sin(M_PI * x / 2.0)) / (M_PI * M_PI * x * x);
+	if (x <= -a || x >= a)
+		return 0.0;
+	return (a * sin(M_PI * x) * sin(M_PI * x / a)) / (M_PI * M_PI * x * x);
 }
 
 void main(void) {
 	vec2 ts = 1.0 / texSize;
 	vec2 iuv = floor(texCoord * texSize) + 0.5;
-	vec2 fuv = -(fract(texCoord * texSize) - 0.5);
+	vec2 fuv = fract(texCoord * texSize) - 0.5;
 	vec3 c0 = texture2D(sTexture, iuv * ts).rgb;
 	vec3 cN = texture2D(sTexture, (iuv + vec2(0.0, -1.0)) * ts).rgb;
 	vec3 cS = texture2D(sTexture, (iuv + vec2(0.0, 1.0)) * ts).rgb;
@@ -28,14 +31,14 @@ void main(void) {
 	vec3 cSE = texture2D(sTexture, (iuv + vec2(1.0, 1.0)) * ts).rgb;
 
 	float w0 = lanc2(length(fuv));
-	float wN = lanc2(length(fuv + vec2(0.0, -1.0)));
-	float wS = lanc2(length(fuv + vec2(0.0, 1.0)));
-	float wW = lanc2(length(fuv + vec2(-1.0, 0.0)));
-	float wE = lanc2(length(fuv + vec2(1.0, 0.0)));
-	float wNW = lanc2(length(fuv + vec2(-1.0, -1.0)));
-	float wNE = lanc2(length(fuv + vec2(1.0, -1.0)));
-	float wSW = lanc2(length(fuv + vec2(-1.0, 1.0)));
-	float wSE = lanc2(length(fuv + vec2(1.0, 1.0)));
+	float wN = lanc2(length(fuv - vec2(0.0, -1.0)));
+	float wS = lanc2(length(fuv - vec2(0.0, 1.0)));
+	float wW = lanc2(length(fuv - vec2(-1.0, 0.0)));
+	float wE = lanc2(length(fuv - vec2(1.0, 0.0)));
+	float wNW = lanc2(length(fuv - vec2(-1.0, -1.0)));
+	float wNE = lanc2(length(fuv - vec2(1.0, -1.0)));
+	float wSW = lanc2(length(fuv - vec2(-1.0, 1.0)));
+	float wSE = lanc2(length(fuv - vec2(1.0, 1.0)));
 
 	vec3 col = c0 * w0 + cN * wN + cS * wS + cW * wW + cE * wE;
 	col += cNW * wNW + cNE * wNE + cSW * wSW + cSE * wSE;
